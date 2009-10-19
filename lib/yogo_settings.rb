7a59771baf778370b0d1d06220abd190924ace6f
Robbie Lamb <robbie.lamb@gmail.com>
2009-10-19 12:43:48 -0600

    Initial Commit.

---
+++ lib/yogo_settings.rb
@@ -0,0 +1,6 @@
+# Yogo Settings Module
+# Copyright (c) 2009 Montana State University
+#
+# FILE: yogo_settings.rb
+# Implementing a settings configuration class.
+require File.dirname(__FILE__) + '/yogo/settings'
