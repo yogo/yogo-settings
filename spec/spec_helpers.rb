344bdcb4f36ecc9a1547c0f096922bcff71a4876
Robbie Lamb <robbie.lamb@gmail.com>
2009-12-28 13:50:53 -0700

    Added in rspec tests.

--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -3,5 +3,7 @@ require 'dm-core'
 
 require File.dirname(__FILE__) + '/../lib/yogo_settings'
 
+Yogo::Settings.load_defaults(File.dirname(__FILE__)+'/test_settings.yml')
+
 DataMapper.setup(:default, 'sqlite3:memory')
 
